53b116e19c63ce04768e41f600eb268bbde6c8b3
rmd09 <123485868+rmd09@users.noreply.github.com>
2023-03-04 23:07:31 +0300

    Code

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program 
+{
+
+    static string InPutHandler(string[] input)
+    {
+
+        //Основной код 
+ 
+        //Или код по умолчанию:
+        string result = string.Empty;
+        foreach (string line in input)
+        {
+            result += line + "\n";
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+    static string pathInput = $"{Environment.CurrentDirectory}\\input.txt";
+    static string pathOutput = $"{Environment.CurrentDirectory}\\output.txt";
+    public static void Main()
+    {
+        string[] input = File.ReadAllLines(pathInput);
+        if (File.Exists(pathOutput))
+        {
+            File.Delete(pathOutput);
+        }
+        string output = InPutHandler(input);
+        File.WriteAllText(pathOutput, output);
+        Console.WriteLine(output);
+    }
+}
